1b0d6cfdfa957c3d235e9104a103e6297d3bbfa6
associate <associate@amkytech.com>
2018-04-13 15:40:49 -0400

    Add existing file

---
+++ updateedit.sh
@@ -0,0 +1,29 @@
+OUT=$(curl https://http://demo9159024.mockable.io/getall)
+
+instid=( $(jq -r '.[].instance_id' <<< "$OUT") )
+
+expdate=( $(jq -r '.[].expirationDate'  <<< "$OUT") )
+
+termdate=( $(jq -r '.[].terminationDate'  <<< "$OUT") )
+
+purpose=( $(jq -r '.[].purpose'  <<< "$OUT") )
+
+project=( $(jq -r '.[].project'  <<< "$OUT") )
+
+environment=( $(jq -r '.[].environment'  <<< "$OUT") )
+
+instanceowner=( $(jq -r '.[].instance_owner' <<< "$OUT") )
+
+n=${#instid[@]}
+
+echo "$n"
+
+for ((i=0;i<n;i++))
+
+do
+
+#echo "aws ec2 create-tags --resources "${arr[$i]}" --tags Key=Term_date,Val$
+
+echo "aws ec2 create-tags --resources "${instid[$i]}" --tags '[ {\"Key\": \"Term_date\", \"Value\":\""${termdate[$i]}"\"},{\"Key\": \"EXP_date\", \"Value\":$
+
+done
